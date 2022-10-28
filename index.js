76ca62c2dd33708b76bab84512a55002cb248a02
Ethan Ochoa <ethanochoa@Ethans-MacBook-Pro.local>
2022-10-28 19:11:07 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import { MongoClient } from 'mongodb'
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+const client = new MongoClient(process.env.MONGO_URI)
+client.connect()
+console.log('mongo connected')
+const PORT = process.env.PORT
+const database = client.db('STS')
+const userLogdb = database.collection('Logs')
+app.listen(PORT, () => console.log('API listening on port 3000'))
+
+
+
+app.get("/get", async (req, res) => {
+    // console.log(req.body);
+    // await userLogdb.insertOne(req.body);
+    const allLogs = await userLogdb.find().toArray();
+    res.send(allLogs);
+})
+
+//           Log Post
+
+app.post("/logs", async (req, res) => {
+    console.log(req.body);
+    await userLogdb.insertOne(req.body);
+    res.json(" Added ");
+  });
+
+
+
+
+//          Log Delete
+
+app.delete("/", async (req, res) => {
+  await userLogdb.findOneAndDelete(req.query)
+  res.json('Be gone fool')
+})
+
+
+//          Log Update
