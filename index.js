c530521d5490eddb559e2bc31b592ce5c364fd75
Ethan Ochoa <ethanochoa@Ethans-MacBook-Pro.local>
2022-10-29 01:58:26 -0400

    third push

--- index.js
+++ index.js
@@ -6,22 +6,21 @@ import { MongoClient } from 'mongodb'
 const app = express()
 app.use(cors())
 app.use(express.json())
-const client = new MongoClient(process.env.MONGO_URI)
-client.connect()
+const URI = process.env.MONGO_URI
+const client = new MongoClient(URI)
 console.log('mongo connected')
 const PORT = process.env.PORT
 const database = client.db('STS')
+client.connect()
 const userLogdb = database.collection('Logs')
 app.listen(PORT, () => console.log('API listening on port 3000'))
 
 
-
-app.get("/get", async (req, res) => {
-    // console.log(req.body);
-    // await userLogdb.insertOne(req.body);
-    const allLogs = await userLogdb.find().toArray();
-    res.send(allLogs);
-})
+app.get('/', async (req, res) => {
+    const allLogs = await userLogdb.find().toArray()
+    res.send(allLogs)
+    console.log(userLogdb)
+  })
 
 //           Log Post
 
